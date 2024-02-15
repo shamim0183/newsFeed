0e631ad1de480515a679e36c30141cf3b01898c7
shamim0183 <shaamim.hossaain@gmail.com>
2024-02-15 22:06:09 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from "./components/Header";
 import News from "./components/News";
 import { NewsProvider } from './context/NewsContext';
+import Footer from './components/Footer';
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -11,6 +12,7 @@ function App() {
       <>
         <Header setSelectedCategory={setSelectedCategory} />
         <News selectedCategory={selectedCategory} />
+        <Footer />
       </>
     </NewsProvider>
   );
